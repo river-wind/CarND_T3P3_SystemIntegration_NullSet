121f069fcd9b1e80d9f782625d8fc308ba488a48
Liam <liam.bowers@adp.com>
2017-12-10 15:52:41 -0500

    feature: first pass at waypoint updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
-import rospy
-from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane, Waypoint
-from std_msgs.msg import Bool
 import math
+import rospy
+import tf
+
+from copy import deepcopy
+from geometry_msgs.msg import PoseStamped
+from styx_msgs.msg import Lane
+from std_msgs.msg import Int32, Bool
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -20,9 +23,30 @@ as well as to verify your TL classifier.
 
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
-# will add new vars here
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
 
+LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+METERS_PER_SECOND_MULTIPLE = 0.44704
+TARGET_VELOCITY_MPH = 10.0
+TARGET_VELOCITY_MPS = TARGET_VELOCITY_MPH * METERS_PER_SECOND_MULTIPLE
+MAXIMUM_ANGLE = math.pi / 4
+
+def waypoint_is_feasible(pose, waypoint):
+    """
+    Determine whether the angle from the current
+    vehicle pose to the given waypoint is drivable
+    """
+    waypoint_x = waypoint.pose.pose.position.x
+    waypoint_y = waypoint.pose.pose.position.y
+    heading_to_waypoint = math.atan2((waypoint_y - pose.position.y), (waypoint_x - pose.position.x))
+    _, _, yaw = tf.transformations.euler_from_quaternion((pose.orientation.x,
+                                                          pose.orientation.y,
+                                                          pose.orientation.z,
+                                                          pose.orientation.w,))
+    return abs(yaw - heading_to_waypoint) <= MAXIMUM_ANGLE
+
+def get_distance(p1, p2):
+    x, y, z = p1.x - p2.x, p1.y - p2.y, p1.z - p2.z
+    return math.sqrt(x*x + y*y + z*z)
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -30,42 +54,54 @@ class WaypointUpdater(object):
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb, queue_size=1) #sub to topic so we can see if dbw is active 
 
-        self.dbw_enabled = True  #default the state to true; we'll update it later
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        # rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below   
-        # set some placeholders
         self.waypoints = None
         self.current_pose = None
+        self.pose_frame_id = None
         self.next_waypoint_index = None
+        self.traffic_light_index = -1
+
         self.loop()
+    
+    def is_ready(self):
+        return all((self.waypoints, self.current_pose,))
 
     def loop(self):
         rate = rospy.Rate(10)
-        start_time = 0
-
-        while not start_time:
-            start_time = rospy.Time.now().to_sec()
 
         while not rospy.is_shutdown():
             rate.sleep()
-            
-    def dbw_enabled_cb(self, msg):
-        self.dbw_enabled = msg.data
-        pass
-    
-    def pose_cb(self, msg):
-        # TODO: Implement
-        pass
 
-    def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+            if not self.is_ready():
+                continue
+
+            lookahead_waypoints = self.get_lookahead_waypoints()
+
+            if self.traffic_light_index == -1:
+                for waypoint in lookahead_waypoints:
+                    waypoint.twist.twist.linear.x = TARGET_VELOCITY_MPS
+
+            self.publish(lookahead_waypoints)
+
+    def publish(self, waypoints):
+        lane = Lane()
+        lane.header.frame_id = self.pose_frame_id
+        lane.header.stamp = rospy.Time.now()
+        lane.waypoints = waypoints
+        self.final_waypoints_pub.publish(lane)
+
+    def pose_cb(self, msg):
+        self.current_pose = msg.pose
+        self.pose_frame_id = msg.header.frame_id
+
+    def waypoints_cb(self, msg):
+        self.waypoints = msg.waypoints
+        rospy.loginfo('Waypoints Received')
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -74,20 +110,24 @@ class WaypointUpdater(object):
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
+    
+    def get_lookahead_waypoints(self):
+        next_waypoint_index = self.get_next_waypoint_index()
+        last_waypoint_index = next_waypoint_index + LOOKAHEAD_WPS
+        return deepcopy(self.waypoints[next_waypoint_index:last_waypoint_index])
 
-    def get_waypoint_velocity(self, waypoint):
-        return waypoint.twist.twist.linear.x
+    def get_next_waypoint_index(self):
+        nearest_distance = 2e32
+        next_waypoint_index = 0
+        for index, waypoint in enumerate(self.waypoints):
+            distance = get_distance(self.current_pose.position, waypoint.pose.pose.position)
+            if (distance < nearest_distance):
+                nearest_distance = distance
+                next_waypoint_index = index
 
-    def set_waypoint_velocity(self, waypoints, waypoint, velocity):
-        waypoints[waypoint].twist.twist.linear.x = velocity
-
-    def distance(self, waypoints, wp1, wp2):
-        dist = 0
-        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-        for i in range(wp1, wp2+1):
-            dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
-            wp1 = i
-        return dist
+        if waypoint_is_feasible(self.current_pose, self.waypoints[next_waypoint_index]):
+            return next_waypoint_index
+        return next_waypoint_index + 1
 
 
 if __name__ == '__main__':
