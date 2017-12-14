0ba4081fdaae2fefef2fe29a3b30288016ccd595
Scott Pillow <scott.pillow@gmail.com>
2017-12-14 03:50:24 -0800

    first cut at JMT related velocity setting

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -13,6 +13,7 @@ import yaml
 
 import numpy as np
 from lowpass import LowPassFilter
+from bisect import bisect_right
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -31,6 +32,9 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 
 LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
 MAXIMUM_ANGLE = math.pi / 4
+
+MAX_ACCEL = 10 # m/s^2
+MAX_JERK  = 10 # m/s^2
 
 def waypoint_is_feasible(pose, waypoint):
     """
@@ -53,6 +57,122 @@ def get_distance(p1, p2):
 def get_dist(p1, p2):
     x, y = p1[0] - p2[0], p1[1] - p2[1]
     return math.sqrt(x*x + y*y)
+
+def velocity_search(start, end, t0, tf, cost_fn):
+    step = (tf - t0) / 20.0
+
+    min_cost = float('+infinity')
+    best_coeffs = None
+    best_time = None
+    curr_time = t0
+    while curr_time <= tf:
+        coeffs = JMT(start, end, curr_time)
+        curr_cost = cost_fn(coeffs, curr_time)
+        if curr_cost < min_cost:
+            best_coeffs = coeffs
+            min_cost = curr_cost
+            best_time = curr_time
+        curr_time += step
+
+    return (best_coeffs, best_time, min_cost)
+
+def JMT(start, end, T):
+    """
+    Calculates Jerk Minimizing Trajectory for start, end and T.
+    """
+    a_0, a_1, a_2 = start[0], start[1], start[2] / 2.0
+    c_0 = a_0 + a_1 * T + a_2 * T**2
+    c_1 = a_1 + 2* a_2 * T
+    c_2 = 2 * a_2
+
+    A = np.array([
+            [  T**3,   T**4,    T**5],
+            [3*T**2, 4*T**3,  5*T**4],
+            [6*T,   12*T**2, 20*T**3],
+        ])
+    B = np.array([
+            end[0] - c_0,
+            end[1] - c_1,
+            end[2] - c_2
+        ])
+    a_3_4_5 = np.linalg.solve(A,B)
+    alphas = np.concatenate([np.array([a_0, a_1, a_2]), a_3_4_5])
+    return alphas
+
+def poly_deriv(coeffs):
+    new_coeffs = []
+    for i in range(1, len(coeffs)):
+        new_coeffs.append(i*coeffs[i])
+
+    return new_coeffs
+
+def poly_eval(coeffs, t):
+    v = 1
+    tot = 0
+    for c in coeffs:
+        tot += v * c
+        v *= t
+    return tot
+
+def cost_jerk_accel(coeffs, T):
+    vt = poly_deriv(coeffs)
+    at = poly_deriv(vt)
+    jt = poly_deriv(at)
+
+    steps = T / 10.0
+    curr_t = 0
+    while curr_t <= T:
+        if poly_eval(at, curr_t) >= MAX_ACCEL - 5:
+            return float('+infinity')
+        if poly_eval(jt, curr_t) >= MAX_JERK - 5:
+            return float('+infinity')
+        curr_t += steps
+
+    return 0
+
+# https://docs.python.org/2/library/bisect.html
+def find_le(a, x):
+    'Find rightmost value less than or equal to x'
+    i = bisect_right(a, x)
+    if i:
+        return i-1
+    return None
+
+def approx_waypoint_times(dists, coeffs, T):
+    step = T / (LOOKAHEAD_WPS * 4.0)
+    curr_t = 0.0
+    times = []
+    while curr_t <= T:
+        times.append(curr_t)
+        curr_t += step
+    times.append(T)
+
+    samples = []
+    for t in times:
+        samples.append(poly_eval(coeffs, t))
+
+    # check sorted
+    for i in range(1, len(samples)):
+        if samples[i-1] > samples[i]:
+            pass
+            #rospy.logwarn("s0 = {0}, s1 = {1}".format(samples[i-1], samples[i]))
+            #assert False
+
+    end_x = samples[-1]
+    dists = [x for x in dists if x <= end_x]
+
+    waypoint_times = []
+
+    for d in dists:
+        i = find_le(samples, d)
+        if samples[i] == d:
+            t = times[i]
+        else:
+            t = (times[i] + times[i+1])/2.0 # TODO: weight it
+
+        waypoint_times.append(t)
+
+    return waypoint_times
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -120,6 +240,10 @@ class WaypointUpdater(object):
         'Walk through the waypoints and calculate distances in between'
 
         dist = get_distance(curr_pose.position, waypoints[0].pose.pose.position)
+
+        total = dist
+
+        tot_distances = [total]
         distances = [dist]
 
         for i in range(1, len(waypoints)):
@@ -127,63 +251,62 @@ class WaypointUpdater(object):
             wp2_pos = waypoints[i].pose.pose.position
             dist = get_distance(wp1_pos, wp2_pos)
 
+            total += dist
+
             distances.append(dist)
+            tot_distances.append(total)
 
-        return distances
+        return (distances, tot_distances)
 
-    def JMT(self, start, end, T):
-        """
-        Calculates Jerk Minimizing Trajectory for start, end and T.
-        """
-        a_0, a_1, a_2 = start[0], start[1], start[2] / 2.0
-        c_0 = a_0 + a_1 * T + a_2 * T**2
-        c_1 = a_1 + 2* a_2 * T
-        c_2 = 2 * a_2
-
-        A = np.array([
-                [  T**3,   T**4,    T**5],
-                [3*T**2, 4*T**3,  5*T**4],
-                [6*T,   12*T**2, 20*T**3],
-            ])
-        B = np.array([
-                end[0] - c_0,
-                end[1] - c_1,
-                end[2] - c_2
-            ])
-        a_3_4_5 = np.linalg.solve(A,B)
-        alphas = np.concatenate([np.array([a_0, a_1, a_2]), a_3_4_5])
-        return alphas
-
-    def update_waypoint_velocities(self, lookahead_waypoints):
+    def update_waypoint_velocities(self, lookahead_waypoints, wp0, wpf):
         if len(lookahead_waypoints) == 0:
             return
 
-        dists = self.piecewise_distance(self.current_pose, lookahead_waypoints)
-        curr_pos = self.current_pose.position
+        (dists, tot_dists) = self.piecewise_distance(self.current_pose, lookahead_waypoints)
 
-        accel = 3.0
+        curr_pos = self.current_pose.position
         v0 = self.current_velocity.linear.x
 
-        #for q in range(10000):
-        #    stuffing = self.JMT([1,2,3],[4,5,6],8)
-
         if self.traffic_light_index != -1:
-            wp_pos = self.waypoints[self.traffic_light_index].pose.pose.position
-            dist_to_stop_line = get_distance(curr_pos, wp_pos)
-            if dist_to_stop_line < 70:
-                accel = -0.5*v0*v0 / dist_to_stop_line
+            if wp0 <= self.traffic_light_index <= wpf:
+                # light coming up
+                stop_line_offset = self.traffic_light_index - wp0
+                dist_from_stop_line = tot_dists[stop_line_offset]
+                start = [0, v0, self.accel_estimate]
+                end   = [dist_from_stop_line, 0, 0]
+                rospy.logwarn("d = {0}, v0 = {1}, a = {2}".format(
+                    dist_from_stop_line, v0, self.accel_estimate))
+                (coeffs, T, cost) = velocity_search(start, end, 0.5, 8, cost_jerk_accel)
+                rospy.logwarn("coeffs = {0}, T = {1}".format(coeffs, T))
+                Ts = approx_waypoint_times(tot_dists, coeffs, T)
+                vt = poly_deriv(coeffs)
+                for (i, t) in enumerate(Ts):
+                    curr_v = poly_eval(vt, t)
+                    lookahead_waypoints[i].twist.twist.linear.x = curr_v
+                del lookahead_waypoints[len(vt):]
+            else:
+                pass
+                # can't see yet
 
-        for i in range(len(lookahead_waypoints)):
-            dx = dists[i]
-            radicand = v0*v0 + 2*accel*dx
-            vf = math.sqrt(radicand) if radicand > 0 else 0
-            vf = min(self.target_velocity, vf)
-            lookahead_waypoints[i].twist.twist.linear.x = vf
-            v0 = vf
+        #accel = 3.0
+        #if self.traffic_light_index != -1:
+        #    wp_pos = self.waypoints[self.traffic_light_index].pose.pose.position
+        #    dist_to_stop_line = get_distance(curr_pos, wp_pos)
+        #    if dist_to_stop_line < 70:
+        #        accel = -0.5*v0*v0 / dist_to_stop_line
+
+        #for i in range(len(lookahead_waypoints)):
+        #    dx = dists[i]
+        #    radicand = v0*v0 + 2*accel*dx
+        #    vf = math.sqrt(radicand) if radicand > 0 else 0
+        #    vf = min(self.target_velocity, vf)
+        #    lookahead_waypoints[i].twist.twist.linear.x = vf
+        #    v0 = vf
 
         min_vel = lookahead_waypoints[0].twist.twist.linear.x
         max_vel = lookahead_waypoints[-1].twist.twist.linear.x
-        rospy.logwarn("curr_vel = {0} m/s, min = {1} m/s, max = {2} m/s".format(self.current_velocity.linear.x, min_vel, max_vel))
+        #rospy.logwarn("curr_vel = {0} m/s, min = {1} m/s, max = {2} m/s, len = {3}".format(
+        #    self.current_velocity.linear.x, min_vel, max_vel, len(lookahead_waypoints)))
 
     def loop(self):
         rate = rospy.Rate(10)
@@ -194,9 +317,13 @@ class WaypointUpdater(object):
             if not self.is_ready():
                 continue
 
-            lookahead_waypoints = self.get_lookahead_waypoints()
+            next_waypoint_index = self.get_next_waypoint_index()
+            last_waypoint_index = next_waypoint_index + LOOKAHEAD_WPS
+            #rospy.logwarn("wp_s = {0}, wp_f = {1}".format(next_waypoint_index, last_waypoint_index))
+            lookahead_waypoints = deepcopy(self.waypoints[next_waypoint_index:last_waypoint_index])
 
-            self.update_waypoint_velocities(lookahead_waypoints)
+            self.update_waypoint_velocities(
+                lookahead_waypoints, next_waypoint_index, last_waypoint_index-1)
 
             self.publish(lookahead_waypoints)
 
@@ -283,13 +410,6 @@ class WaypointUpdater(object):
         curr_accel = dv / dt
         self.accel_estimate = self.accel_filter.filt(curr_accel)
         #rospy.logwarn("accel estimate = {0}".format(self.accel_estimate))
-
-
-    def get_lookahead_waypoints(self):
-        next_waypoint_index = self.get_next_waypoint_index()
-        last_waypoint_index = next_waypoint_index + LOOKAHEAD_WPS
-        #rospy.logwarn("wp_s = {0}, wp_f = {1}".format(next_waypoint_index, last_waypoint_index))
-        return deepcopy(self.waypoints[next_waypoint_index:last_waypoint_index])
 
     def get_next_waypoint_index(self):
         nearest_distance = 2e32
